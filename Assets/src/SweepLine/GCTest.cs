8edfa08845ff5f5ee5f90ebae0124b73bde55f4c
Andrew <mailtoandrew@qq.com>
2019-12-27 17:29:17 +0800

    22

---
+++ Assets/src/SweepLine/GCTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SweepLine;
+using UnityEngine;
+
+public class GCTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		for(int i = 0; i < 100; i++)
+		{
+			Point2 _p = new Point2(Random.Range(0, 10), Random.Range(0, 10));
+			if(!m_testTable.ContainsKey(_p))
+			{
+				m_testTable.Add(_p, _p);
+			}
+		}
+	}
+    Dictionary<Point2, Point2> m_testTable=new Dictionary<Point2, Point2>();//new Point2.Comparer()
+																			// Update is called once per frame
+	void Update()
+    {
+	    for (int i = 0; i < 100; i++)
+	    {
+			Point2 _p = new Point2(Random.Range(0, 10), Random.Range(0, 10));
+			if(!m_testTable.ContainsKey(_p))
+			{
+				m_testTable.Add(_p, _p);
+			}
+			else
+			{
+				m_testTable.Remove(_p);
+			}
+		}
+
+    }
+}
--- Assets/src/SweepLine/Point2.cs
+++ Assets/src/SweepLine/Point2.cs
@@ -3,67 +3,78 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace SweepLine
 {
-    public struct Point2:IComparable<Point2>
-    {
-        private static int IncID = 0;
-        private int m_id;
-        public float X;
-        public float Y;
-        public Point2(float _x, float _y)
-        {
-            m_id = IncID++;
-            X = _x;
-            Y = _y;
-        }
-        public Point2(Vector2 _point)
-        {
-            m_id = IncID++;
-            X = _point.x;
-            Y = _point.y;
-        }
-        public void SetValue(Vector2 _point)
-        {
-            if (m_id == 0)
-            {
-                m_id = IncID++;
-            }
-            X = _point.x;
-            Y = _point.y;
-        }
-        public Vector2 getValue()
-        {
-            return new Vector2(X, Y);
-        }
-
-        public override String ToString()
-        {
-	        return "(" + X + "," + Y + ")";
-        }
-        public int CompareTo(Point2 other)
-        {
-	        if (X != other.X)
-	        {
-		        return X > other.X ? 1 : -1;
+	public struct Point2 : IComparable<Point2>, IEquatable<Point2>
+	{
+		private static int IncID = 0;
+		private int m_id;
+		public float X;
+		public float Y;
+		public Point2(float _x, float _y)
+		{
+			m_id = IncID++;
+			X = _x;
+			Y = _y;
+		}
+		public Point2(Vector2 _point)
+		{
+			m_id = IncID++;
+			X = _point.x;
+			Y = _point.y;
+		}
+		public void SetValue(Vector2 _point)
+		{
+			if(m_id == 0)
+			{
+				m_id = IncID++;
 			}
-	        if(Y != other.Y)
-	        {
-		        return Y > other.Y ? 1 : -1;
-	        }
-	        return 0;
-        }
-		public struct Comparer : IEqualityComparer<Point2>
-        {
-            bool IEqualityComparer<Point2>.Equals(Point2 x, Point2 y)
-            {
-                return x.Equals(y);
-            }
+			X = _point.x;
+			Y = _point.y;
+		}
+		public Vector2 getValue()
+		{
+			return new Vector2(X, Y);
+		}
+		public int CompareTo(Point2 other)
+		{
+			if(X != other.X)
+			{
+				return X > other.X ? 1 : -1;
+			}
+			if(Y != other.Y)
+			{
+				return Y > other.Y ? 1 : -1;
+			}
+			return 0;
+		}
+		public static bool operator ==(Point2 left, Point2 right)
+		{
+			return left.Equals(right);
+		}
 
-            int IEqualityComparer<Point2>.GetHashCode(Point2 obj)
-            {
-                return obj.GetHashCode();
-            }
-        }
+		public static bool operator !=(Point2 left, Point2 right)
+		{
+			return !(left == right);
+		}
+		public bool Equals(Point2 other)
+		{
+			return CompareTo(other) == 0;
+		}
+		public override bool Equals(object obj)
+		{
+			return obj is Point2 other && Equals(other);
+		}
+		public override int GetHashCode()
+		{
+			int hash = 13;
+			hash = (hash * 7) + X.GetHashCode();
+			hash = (hash * 7) + Y.GetHashCode();
+			return hash;
+		}
+		public override String ToString()
+		{
+			return "(" + X + "," + Y + ")";
+		}
 
 
-    }
+	}
 }
